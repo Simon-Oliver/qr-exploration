c9ee02a9920065ed14600e3752425b6d212fcc12
Simon-Oliver <stauffer.simon@outlook.com>
2019-11-27 22:21:29 +0100

    server side qr reading

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+var Jimp = require('jimp');
+var fs = require('fs');
+const jsQR = require('jsqr');
+
 const app = express();
 const port = 3000;
-const cors = require('cors');
-var multer = require('multer');
 
 app.use(
   cors({
@@ -16,7 +20,7 @@ app.use(express.urlencoded());
 
 var storage = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, 'files/');
+    cb(null, 'files/temp');
   },
   filename: function(req, file, cb) {
     cb(null, file.originalname); // modified here  or user file.mimetype
@@ -32,7 +36,25 @@ app.post('/img', upload.single('image'), (req, res, next) => {
   console.log('/img FIRED');
   console.log(req.file);
   // req.body will hold the text fields, if there were any
-  res.send(200);
+  console.log(__dirname);
+  var buffer = fs.readFileSync(__dirname + '/' + req.file.path);
+
+  Jimp.read(buffer, async function(err, image) {
+    const value = jsQR(image.bitmap.data, image.bitmap.width, image.bitmap.height);
+    const data = JSON.parse(value.data);
+
+    console.log(data);
+    res.json({ ...data });
+  });
+
+  fs.unlink(__dirname + '/' + req.file.path, err => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    //file removed
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
